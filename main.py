2cae50ec41354b5e4c1ce3789df914d87ede5fee
himesh <itsmee257@gmail.com>
2019-05-02 09:57:14 -0400

    almost done

--- main.py
+++ main.py
@@ -5,27 +5,35 @@ import random, json
 app = Flask(__name__)
 
 carss = ['honda', 'hundai', 'toyota']
-url = "http://www.google.com"
+url = ["http://www.google.com", "https://www.google.com/search?q=bootstrap+footers&oq=boo&aqs=chrome.0.69i59j69i60j69i65l2j69i57j69i59.799j0j7&sourceid=chrome&ie=UTF-8", "https://getbootstrap.com/docs/4.1/examples/"]
+bl = [];
+
 
 @app.route('/')
 def mainPage():
     print("im at mainpage")
     return render_template("main.html", results = carss, url = url, c = len(carss))
-    
-#@app.route('/b')
-#def bucketListss():
-#    print('bl page')
-#    return render_template("bucketList.html", cars = carss, c = len(carss), url = url)
 
 @app.route('/bucketList', methods=['GET','POST'])
 def bucketLists():
     print('bl page')
-    if request.method == 'POST':
-        return render_template("bucketList.html")
-    return render_template("bucketList.html")
+    return render_template("bucketList.html", bl = bl, c = len(bl))
     
 @app.route('/final')
 def addBtn():
+    a = request.args.get('a')
+    bl.append(a)
+    print(bl)
+    return a
+    
+@app.route('/pdf')
+def pdf():
+    a = request.args.get('a')
+    print(a)
+    return a
+    
+@app.route('/url')
+def url():
     a = request.args.get('a')
     print(a)
     return a
