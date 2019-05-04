cddc3bae7969bbbfb3c32a0edb8853d7b6e9e1c0
himesh <itsmee257@gmail.com>
2019-05-04 16:48:42 -0400

    not there yet

--- main.py
+++ main.py
@@ -1,19 +1,44 @@
 import sys
-from flask import Flask, render_template, request, jsonify
+import time
+import os
+from flask import Flask, render_template, request, session, redirect, url_for
 import random, json
+try: 
+    from googlesearch import search 
+except ImportError:  
+    print("No module named 'google' found") 
 
 app = Flask(__name__)
-
-carss = ['honda', 'hundai', 'toyota']
-url = ["http://www.google.com", "https://www.google.com/search?q=bootstrap+footers&oq=boo&aqs=chrome.0.69i59j69i60j69i65l2j69i57j69i59.799j0j7&sourceid=chrome&ie=UTF-8", "https://getbootstrap.com/docs/4.1/examples/"]
+app.secret_key = os.urandom(24)
+#carss = ['honda', 'hundai', 'toyota']
+#url = ["http://www.google.com", "https://www.google.com/search?q=bootstrap+footers&oq=boo&aqs=chrome.0.69i59j69i60j69i65l2j69i57j69i59.799j0j7&sourceid=chrome&ie=UTF-8", "https://getbootstrap.com/docs/4.1/examples/"]
 bl = [];
+results = [];
+resultsUrl = [];
+one = '' 
+ 
+def res(q):
+    query = q
+    for j in search(query, tld="co.in", num=1, stop=2, pause=2): 
+        #print(j)
+        results.append(j)
+    return results
+    
+def resUrl(q):
+    query = q
+    for j in search(query, tld="co.in", num=1, stop=1, pause=2): 
+        #print(j)
+        resultsUrl.append(j)
+    return resultsUrl
 
 
 @app.route('/')
 def mainPage():
     print("im at mainpage")
-    return render_template("main.html", results = carss, url = url, c = len(carss))
-
+    a = request.args.get('a')
+    #return render_template("main.html", results = res(a), url = url, c = len(x))
+    return render_template("main.html", results = [], url = url, c = 0)
+    
 @app.route('/bucketList', methods=['GET','POST'])
 def bucketLists():
     print('bl page')
@@ -25,20 +50,38 @@ def addBtn():
     bl.append(a)
     print(bl)
     return a
-    
-@app.route('/pdf')
+
+@app.route('/pdf', methods=['GET','POST'])
 def pdf():
-    a = request.args.get('a')
-    print(a)
-    return a
+    start = time.time()
+    print('pdf page')
+    a = request.args.get('a')   
+    print(res(a))
+    print(results)
+    end = time.time()
+    one = end - start
+    #print(end - start)
+    return "ok"
     
+    
+@app.route('/pdfs', methods=['GET','POST'])
+def pdfs():
+    time.sleep(4)
+    return render_template("main.html", results = results, url = results, c = len(results))
+
 @app.route('/url')
 def url():
-    a = request.args.get('a')
-    print(a)
-    return a
+    print('url page')
+    a = request.args.get('a')   
+    print(resUrl(a))
+    print(resultsUrl)
+    return "ok"
     
-    
+@app.route('/urls', methods=['GET','POST'])
+def urls():
+    print(results)
+    return render_template("main.html", results = resultsUrl, url = results, c = len(resultsUrl))
+
     
  
     
