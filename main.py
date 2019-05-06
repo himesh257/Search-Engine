f7f28e833a82eb4610ea336a39e7269f3d90231f
himesh <itsmee257@gmail.com>
2019-05-05 20:16:25 -0400

    Finally functioning thnx to Threading

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 import subprocess
 import os
 from flask import Flask, render_template, request, session, redirect, url_for
+from threading import Thread
 import random, json
 import requests
 from bs4 import BeautifulSoup
@@ -15,7 +16,6 @@ except ImportError:
     print("No module named 'google' found") 
 
 app = Flask(__name__)
-app.secret_key = os.urandom(24)
 
 carss = ['honda', 'hundai', 'toyota','o','o']
 bl = []
@@ -24,6 +24,9 @@ titles = []
 resultsPdf = []
 titlesPdf = []
 ind = []
+timess = []
+threads = []
+threadsPdf = []
 
 def data_title():
     with open('data_titles.txt', 'w') as f:
@@ -31,16 +34,14 @@ def data_title():
         for item in ind:
             f.write("%s\n" % item)
 
-
 def data_url():
     with open('data_url.txt', 'w') as f:
         for item in bl:
             f.write("%s\n" % item)
 
 def res(q):
-    start = time.time()
     query = q
-    for j in search(query, tld="com", num=2, stop=1, pause=3): 
+    for j in search(query, tld="com", num=3, stop=2, pause=3): 
         #print(j)
         results.append(j)
         r = requests.get(j).text
@@ -51,25 +52,23 @@ def res(q):
             titles.append(soup.title.string)
     print(results)
     end = time.time()
+    
     return results
     
 def resPdf(q):
     query = q + " pdf"
-    for j in search(query, tld="com", num=3, stop=5, pause=2): 
+    for j in search(query, tld="com", num=3, stop=2, pause=2): 
         print(j)
         if j[-3:] == "pdf":
             resultsPdf.append(j)
             titlesPdf.append(j.split('/')[2])
-       
     print(resultsPdf)
     print(titlesPdf)
     return resultsPdf
 
-
 @app.route('/')
 def mainPage():
     print("im at mainpage")
-    
     a = request.args.get('a')
     return render_template("main.html", results = [], url = [], c = 0)
     #return render_template("main.html", results = carss, url = url, c = len(carss))
@@ -106,34 +105,43 @@ def addBtn():
 
 @app.route('/url', methods=['GET','POST'])
 def url():
+    a = request.args.get('a')
+    for i in range(0,1):
+        process = Thread(target=res, args=[a])
+        process.start()
+        print(process)
+        threads.append(process)
     print('url page')
-    a = request.args.get('a') 
-    res(a) 
+    #res(a)
+    for process in threads:
+        process.join()
+    print("here")
     return "ok"
     
 @app.route('/urls', methods=['GET','POST'])
 def urls():
-    time.sleep(7)
-    print(results)
+    for process in threads:
+        process.join()
     return render_template("main.html", results = titles, url = results, c = len(results))
 
 @app.route('/pdf', methods=['GET','POST'])
 def pdf():
     print('pdf page')
     a = request.args.get('a') 
-    resPdf(a) 
-    print("--------------")
-    print(resultsPdf)
-    print(titlesPdf)
+    for i in range(0,1):
+        process = Thread(target=resPdf, args=[a])
+        process.start()
+        print(process)
+        threadsPdf.append(process)
+    for process in threadsPdf:
+        process.join()
     return "ok"
     
 @app.route('/pdfs', methods=['GET','POST'])
 def pdfs():
-    time.sleep(7)
-    print(results)
+    for process in threadsPdf:
+        process.join()
     return render_template("main.html", results = titlesPdf, url = resultsPdf, c = len(resultsPdf))
-
-
     
 if __name__ == '__main__':
     app.run(debug=True)
